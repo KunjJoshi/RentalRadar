b5942a98d589710dea9e0c39a7cf92c2bba3f4df
Kunj Joshi <93201673+KunjJoshi@users.noreply.github.com>
2023-07-18 23:58:19 -0700

    Prt 8000

--- app.py
+++ app.py
@@ -82,6 +82,6 @@ def sendemail():
   return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',port='5000',debug=True)
+  app.run(host='0.0.0.0',port='8000',debug=True)
 
 webbrowser.open('http://127.0.0.1:5000')
